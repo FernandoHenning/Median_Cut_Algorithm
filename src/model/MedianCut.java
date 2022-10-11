ff47b7629e7abc1e6f1bcf4bbe7acb0f4a505171
Fernando Henning Macias <fernando.henning@vantis.mx>
2022-10-11 12:30:37 -0500

    Created: Methods execute() and getPixelsData in MedianCut class

--- src/model/MedianCut.java
+++ src/model/MedianCut.java
@@ -1,6 +1,9 @@
 package model;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.stream.IntStream;
 
 public class MedianCut {
     private final BufferedImage img;
@@ -14,5 +17,31 @@ public class MedianCut {
         this.width = img.getWidth();
         this.height = img.getHeight();
         this.depth = depth;
+    }
+
+    public void execute() throws IOException {
+        int[] dataBuffInt = this.img.getRGB(0, 0, this.width, this.height, null, 0, this.width);
+        PixelData[] pixelsData = getPixelsData(dataBuffInt);
+        splitIntoBuckets(pixelsData, this.depth);
+    }
+
+    private void splitIntoBuckets(PixelData[] pixelsData, int depth) {
+    }
+
+    private PixelData[] getPixelsData(int[] bufferedIntRGB) {
+        PixelData[] pixels = new PixelData[this.height * this.width];
+        IntStream.range(0, this.width).parallel()
+                .forEach(
+                        i -> (IntStream.range(0, this.height))
+                                .forEach(j -> {
+                                    int currentIndex = j * this.width + i;
+                                    PixelData pixelData = new PixelData();
+                                    pixelData.color = new Color(bufferedIntRGB[currentIndex]);
+                                    pixelData.x = i;
+                                    pixelData.y = j;
+                                    pixels[currentIndex] = pixelData;
+                                })
+                );
+        return pixels;
     }
 }
